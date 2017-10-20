dce68b21a0897f048fc04e2b0de830d30b1d7bd9
patrzykont <aaronpatrzykont@Aarons-MacBook-Air.local>
2017-10-20 17:09:51 -0500

    message

---
+++ trivia game/app.js
@@ -0,0 +1,115 @@
+$ (document).ready(function() {
+
+	function intialScreen(){
+		startScreen + "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'> Start Quiz</a></p>";
+		$(".mainArea").html(startScreen);
+
+	}
+intialScreen();
+
+$("body").on("click", ".start-button", function(event){
+	event.preventDefault();
+	generateHtml();
+
+	timerWrapper();
+
+});
+$("body").on("click", ".answer", function(event){
+	selectedAnswer = $(this).text();
+	if(selectedAnswer === correctAnswers[questionCounter]){
+
+		clearInterval(theClock);
+		generateWin();
+	}
+	else {
+		clearInterval(theClock);
+		generateLoss();
+
+	}
+});
+
+$("body").on("click", "reset-button", function(event){
+	resetGame();
+});
+
+});
+
+function generateLossDueToTimeOut(){
+	unansweredTally++;
+	gameHTML = "<p class= 'text-center timer-p'>Time Remaining: <span class='tiner'>" + counter + "</span></p>" + "<p class='text-center'>You've run out of time be quicker than that! The correct answer was: " + correctAnswers[questionCounter] + "</p>";
+	$(".mainArea").html(gameHTML);
+	setTimeout(WAIT, 3000);
+}
+
+function generateWin(){
+	unansweredTally++;
+	gameHTML = "<p class= 'text-center timer-p'>Time Remaining: <span class='tiner'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>";
+	$(".mainArea").html(gameHTML);
+	setTimeout(WAIT, 3000);
+}
+
+function generateLoss(){
+	unansweredTally++;
+	gameHTML = "<p class= 'text-center timer-p'>Time Remaining: <span class='tiner'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: " + correctAnswers[questionCounter] + "</p>";
+	$(".mainArea").html(gameHTML);
+	setTimeout(WAIT, 3000);
+}
+
+function generateHTML() {
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+"</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
+	$(".mainArea").html(gameHTML);
+}
+
+function wait(){
+	if (questionCounter < 7){
+		questionCounter++;
+		generateHTML();
+		counter = 30;
+		timerWrapper();
+	}
+	else{
+			finalScreen();
+	}
+}
+
+function timerWrapper(){
+	theClock = setInterval(thritySeconds, 1000);
+	Function thritySeconds(){
+		if (counter === 0){
+			clearInterval(theClock);
+			generateLossDueToTimeOut();
+		}
+		if (counter>0){
+			counter--;
+		}
+		$(".timer").html(counter);
+	}
+}
+
+function finalScreen() {
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>The Game Is Over, Here Are Your Results!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Game!</a></p>";
+	$(".mainArea").html(gameHTML);
+}
+
+function resetGame(){
+	questionCounter = 0;
+	correctTally = 0;
+	incorrectTally = 0;
+	unansweredTally = 0;
+	counter = 30;
+	generateHTML();
+	timerWrapper();
+}
+
+var startScreen;
+var gameHTML;
+var counter = 30;
+var questionArray = ["Who won the NCAA Basketball Championship in 2008?","Who was the 44th President of the United States?","What is the capital of Texas?","Which of the following companies are based out of South Korea?","Which of the following is not a part of the Teenage Mutant Ninja Turtles?","Foghorn Leghorn was what kind of animal?","The Green Bay Packers faced which team in the first Super Bowl?","Jack Spaarow is portrayed by what actor"];
+var answerArray = [["Kansas","Duke","UCLA","Kentucky"],["George W. Bush","Barack Obama","Bill Clinton","Jimmy Carter"],["San Antonio","Dallas","Austin","Houston"],["Toyota","Samsung","COSCO","Yamaha"],["Leonardo","Donatello","Raphael","Mario"],["Rooster","Cat","Dog","Horse"],["Minnesota Vikings","Oakland Raiders","Kansas City Chiefs","Chicago Bears"],["Vince Vaughn","Johnny Depp","George Clooney","Ashton Kutcher"]];
+var correctAnswers = ["A. Kansas","B. Barack Obama","C. Austin","B. Samsung","D. Mario","A. Rooster","C. Kansas City Chiefs","B. Johnny Depp"];
+var questionCounter = 0;
+var selectedAnswer;
+var theClock;
+var correctTally = 0;
+var incorrectTally =0;
+var unansweredTally = 0;
